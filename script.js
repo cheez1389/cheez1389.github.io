65a2bed6de67cae96442de2ae24286179b5f8f61
cheez1389 <kareem.alhalabi@icloud.com>
2024-09-27 19:27:43 -0400

    y

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-    new Splide('#image-carousel', {
-        type       : 'loop',       // Carousel will loop
-        autoplay   : true,         // Enable auto-scroll
-        interval   : 3000,         // Auto-scroll interval (3 seconds)
-        cover      : true,
-        height     : '100%',       // Make sure the height is 100%
-        width      : '100%',       // Make sure the width is 100%
-        arrows     : true,         // Enable navigation arrows
-        pagination : false,        // Disable pagination (dots)
-    }).mount();
+new Splide('#image-carousel', {
+    type       : 'loop',
+    autoplay   : true,
+    interval   : 3000,
+    height     : '100%',
+    width      : '100%',
+    arrows     : true,
+    pagination : false,
+}).mount();
+
 });
