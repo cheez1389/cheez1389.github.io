cd3a5e6006cfd9675cb89ef5c41e24f3d92a0779
cheez1389 <kareem.alhalabi@icloud.com>
2024-09-27 20:26:53 -0400

    reset

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-new Splide('#image-carousel', {
-    type       : 'loop',
-    autoplay   : true,
-    interval   : 3000,
-    height     : '100%',
-    width      : '100%',
-    arrows     : true,
-    pagination : false,
-}).mount();
-
+    console.log('Carousel initialized');
+    new Splide('#image-carousel', {
+        type       : 'loop',
+        autoplay   : true,
+        interval   : 3000,
+        cover      : true,
+        height     : '100%',
+        width      : '100%',
+        arrows     : true,
+        pagination : false,
+    }).mount();
 });
